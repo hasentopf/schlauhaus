f19341f219ae64c8b0af081e8eaad148c53345c8
MvH <assembla@periscope.de>
2025-04-28 15:55:03 +0200

    First try of an IPS module

--- eCPC/module.php
+++ eCPC/module.php
@@ -30,18 +30,6 @@ class eCPC extends IPSModule
     {
         $jsonform = json_decode(file_get_contents(__DIR__."/form.json"), true);
 
-        $value = array();
-        $result = $this->ReadAttributeString("Homes");
-        $this->SendDebug('Homes-Values_Attribute', json_encode($result),0)	;
-        if ($result == '') return;
-        $homes = json_decode($result, true);
-        $value[] = ["caption"=> "", "value"=> "" ];
-        foreach ($homes["data"]["viewer"]["homes"] as $key => $home){
-            $value[] = ["caption"=> $home["appNickname"], "value"=> $home["id"] ];
-        }
-        $this->SendDebug('Homes-Values', json_encode($value),0)	;
-        $jsonform["elements"][2]['items'][0]["options"] = $value;
-        $this->SendDebug('Homes-Values', json_encode($jsonform),0)	;
         return json_encode($jsonform);
     }
 }
