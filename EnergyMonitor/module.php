c86c8989f3992af4f4ed8b0cf9f363503c116ab6
MvH <assembla@periscope.de>
2026-03-23 20:40:41 +0100

    Verbrauch Aus Archiv (Energy Monitoring)

---
+++ EnergyMonitor/module.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../libs/schlauhaus.php';
+
+class VerbrauchAusArchiv extends IPSModule
+{
+    use Schlauhaus;
+
+    private const ROUND_DECIMALS = 2;
+
+    public function Create()
+    {
+        parent::Create();
+
+        $this->SetVisualizationType(1);
+
+        $this->RegisterPropertyInteger('ArchiveId', 0);
+        $this->RegisterPropertyString('Variables', '[]');
+        $this->RegisterPropertyInteger('UpdateInterval', 60);
+        $this->RegisterPropertyBoolean('EarlyMorningUpdate', true);
+        $this->RegisterPropertyInteger('EarlyMorningWindow', 600);
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        $this->RegisterMessage(0, IPS_KERNELSTARTED);
+        $this->RegisterTimer('UpdateTimer', $this->ReadPropertyInteger('UpdateInterval') * 1000, 'VAA_Update($_IPS[\'TARGET\']);');
+
+        if (IPS_GetKernelRunlevel() === KR_READY) {
+            $this->UpdateData();
+        }
+    }
+
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+    {
+        $this->SendDebug('MessageSink', "SenderID: $SenderID, Message: $Message", 0);
+
+        if ($Message === IPS_KERNELSTARTED) {
+            $this->UpdateData();
+        }
+    }
+
+    public function Update()
+    {
+        $this->UpdateData();
+    }
+
+    private function UpdateData()
+    {
+        $archiveId = $this->ReadPropertyInteger('ArchiveId');
+        if ($archiveId === 0) {
+            $instances = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}');
+            $archiveId = $instances[0] ?? 0;
+        }
+
+        if ($archiveId === 0) {
+            $this->LogMessage('No archive instance found', KL_ERROR);
+            return;
+        }
+
+        $variables = json_decode($this->ReadPropertyString('Variables'), true);
+        if (empty($variables)) {
+            $this->LogMessage('No variables configured', KL_WARNING);
+            return;
+        }
+
+        $earlyMorningUpdate = $this->ReadPropertyBoolean('EarlyMorningUpdate');
+        $earlyMorningWindow = $this->ReadPropertyInteger('EarlyMorningWindow');
+        $isEarly = $this->isShortlyAfterMidnight($earlyMorningWindow);
+
+        $data = $this->collectData($archiveId, $variables, $isEarly);
+        $this->saveData($data);
+        $html = $this->generateHtml($data);
+        $this->SetValue('HTMLContent', $html);
+    }
+
+    private function isShortlyAfterMidnight(int $seconds = 300): bool
+    {
+        $now = new DateTimeImmutable('now', new DateTimeZone('UTC'));
+        $midnight = $now->setTime(0, 0, 0);
+        $diff = $now->getTimestamp() - $midnight->getTimestamp();
+        return $diff >= 0 && $diff < $seconds;
+    }
+
+    private function collectData(int $archiveId, array $variables, bool $updateYesterday): array
+    {
+        $data = [];
+
+        foreach ($variables as $var) {
+            $varId = $var['VariableId'];
+            $yesterdayTarget = $var['YesterdayTarget'];
+            $todayTarget = $var['TodayTarget'];
+            $name = $var['Name'];
+            $type = $var['Type'];
+
+            if ($updateYesterday) {
+                $yesterdayData = AC_GetAggregatedValues($archiveId, $varId, 1, strtotime('yesterday'), strtotime('today') - 1, 0);
+                SetValueFloat($yesterdayTarget, $this->calcConsumption($yesterdayData));
+            }
+
+            $todayData = AC_GetAggregatedValues($archiveId, $varId, 1, strtotime('today'), time(), 0);
+            SetValueFloat($todayTarget, $this->calcConsumption($todayData));
+
+            $data[] = [
+                'name' => $name,
+                'type' => $type,
+                'yesterday' => GetValueFloat($yesterdayTarget),
+                'today' => GetValueFloat($todayTarget)
+            ];
+        }
+
+        return $data;
+    }
+
+    private function calcConsumption(array $values): float
+    {
+        $consumption = 0;
+        foreach ($values as $value) {
+            $consumption += $value['Avg'];
+        }
+        return round($consumption, self::ROUND_DECIMALS);
+    }
+
+    private function saveData(array $data): void
+    {
+        $totalYesterday = 0;
+        $totalToday = 0;
+        $totalYesterdayYield = 0;
+        $totalTodayYield = 0;
+
+        foreach ($data as $item) {
+            if ($item['type'] === 'verbrauch') {
+                $totalYesterday += $item['yesterday'];
+                $totalToday += $item['today'];
+            } else {
+                $totalYesterdayYield += $item['yesterday'];
+                $totalTodayYield += $item['today'];
+            }
+        }
+
+        $this->SetValue('TotalYesterday', round($totalYesterday, self::ROUND_DECIMALS));
+        $this->SetValue('TotalToday', round($totalToday, self::ROUND_DECIMALS));
+        $this->SetValue('TotalYesterdayYield', round($totalYesterdayYield, self::ROUND_DECIMALS));
+        $this->SetValue('TotalTodayYield', round($totalTodayYield, self::ROUND_DECIMALS));
+    }
+
+    private function generateHtml(array $data): string
+    {
+        $html = '<table border="1" cellpadding="5" style="border-collapse: collapse; width: 100%; text-align: left;">';
+        $html2 = '<br><table border="1" cellpadding="5" style="border-collapse: collapse; width: 100%; text-align: left;">';
+        $html .= '<tr><th>Hausverbrauch</th><th>Gestern</th><th>Heute</th></tr>';
+        $html2 .= '<tr><th>PV-Ertrag</th><th>Gestern</th><th>Heute</th></tr>';
+
+        $totalYesterday = 0;
+        $totalToday = 0;
+        $totalYesterdayYield = 0;
+        $totalTodayYield = 0;
+
+        foreach ($data as $item) {
+            if ($item['type'] === 'verbrauch') {
+                $html .= '<tr>';
+                $html .= '<td>' . htmlspecialchars($item['name']) . '</td>';
+                $html .= '<td>' . $item['yesterday'] . ' kWh</td>';
+                $html .= '<td>' . $item['today'] . ' kWh</td>';
+                $html .= '</tr>';
+                $totalYesterday += $item['yesterday'];
+                $totalToday += $item['today'];
+            } else {
+                $html2 .= '<tr>';
+                $html2 .= '<td>' . htmlspecialchars($item['name']) . '</td>';
+                $html2 .= '<td>' . $item['yesterday'] . ' kWh</td>';
+                $html2 .= '<td>' . $item['today'] . ' kWh</td>';
+                $html2 .= '</tr>';
+                $totalYesterdayYield += $item['yesterday'];
+                $totalTodayYield += $item['today'];
+            }
+        }
+
+        $html .= '<tr style="font-weight: bold;"><td>Gesamt</td><td>' . round($totalYesterday, self::ROUND_DECIMALS) . ' kWh</td><td>' . round($totalToday, self::ROUND_DECIMALS) . ' kWh</td></tr></table>';
+        $html2 .= '<tr style="font-weight: bold;"><td>Gesamt</td><td>' . round($totalYesterdayYield, self::ROUND_DECIMALS) . ' kWh</td><td>' . round($totalTodayYield, self::ROUND_DECIMALS) . ' kWh</td></tr></table>';
+
+        return $html . $html2;
+    }
+
+    public function GetVisualizationTile()
+    {
+        $module = file_get_contents(__DIR__ . '/module.html');
+        $html = $this->GetValue('HTMLContent');
+        return str_replace('{{CONTENT}}', $html, $module);
+    }
+}
--- README.md
+++ README.md
@@ -10,7 +10,12 @@ Lite Version (only SSH)
     sudo apt update
     sudo apt upgrade -y
 
-    sudo raspi-config # Advanced Options -> Expand Filesystem
+    sudo raspi-config 
+    # Advanced Options → Expand Filesystem
+    # Interface Options → Serial Port                                                                                                                                                                                                                                                                             
+    # Would you like a login shell to be accessible over serial? → Select No                                                                                                                                                                                                                                      
+    # Would you like the serial port hardware to be enabled? → Select Yes
+    sudo reboot        
 
     # Install Docker
     curl -sSL https://get.docker.com | sh
@@ -30,7 +35,7 @@ Lite Version (only SSH)
     docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
     # Get IP Address
     hostname -I
-
+    # Go to https://<IP-Adresse>:9443 and create a admin user for portainer
 
 --------------------------------------------------------
 PORTAINER KONFIGURATION:
@@ -50,11 +55,73 @@ Ports:
     docker volume create symcon_log
     docker volume create symcon_root
 
+Network: host
+
+Volumes:
+    Container: /var/lib/symcon  Host: /home/rasp_user/symcon/data  Bind
+    Container: /var/log/symcon Host: /home/rasp_user/symcon/log  Bind
+    Container: /root                  Host: /home/rasp_user/symcon/license      Bind
+
+TZ: Europe/Berlin
+
+## Backup
+    mkdir ~/bin
+    cd ~/bin
+    vi backup.sh
+    chmod +x backup.sh
+    ./backup.sh
+    mkdir ~/backups
+
+### USB-Stick auf Raspberry PI mounten:
+    sudo fsck.vfat -a /dev/sda1
+    # mount with clean permissions:
+    sudo mount -o uid=1000,gid=1000,umask=000 /dev/sda1 /mnt/usb
+    # make it permanent: 
+    sudo nano /etc/fstab
+    # add this line:
+    /dev/sda1  /mnt/usb  vfat  user,rw,umask=000,uid=1000,gid=1000,nofail  0  0
+    # run in cronjob
+    crontab -e
+    # run it every night at 3:00 AM
+    0 3 * * * /home/rasp_user/backup_z2m.sh 
+
+
 ## eCPC
-
-docker exec -it mosquitto mosquitto_passwd /mosquitto/data/passwd mqttZ2M
-
-mosquitto_pub -h localhost -p 1883 -t /topic -m "Authenticated message" -u username -P password
 
 ExecStart=/home/rasp_local/dht_env/bin/python3
 /home/rasp_local/dht_env/dht_mqtt.py                                            
+
+    pip install paho-mqtt --break-system-packages
+    pip3 install adafruit-circuitpython-dht --break-system-packages
+    python3 dht_mqtt.py    
+
+    sudo nano /etc/systemd/system/dht_mqtt.service
+    [Unit]
+    Description=DHT22 MQTT Publisher
+    After=network.target
+    
+    [Service]
+    User=rasp_user
+    WorkingDirectory=/home/rasp_user/dht_mqtt
+    ExecStart=/usr/bin/python3 /home/rasp_user/dht_mqtt/dht_mqtt.py
+    Restart=always
+    
+    [Install]
+    WantedBy=multi-user.target
+
+    sudo systemctl enable dht_mqtt.service
+    sudo systemctl start dht_mqtt.service
+
+    [Unit]
+    Description=EMH Electricity Meter MQTT
+    Publisher
+    After=network.target
+    
+    [Service]
+    User=rasp_user
+    WorkingDirectory=/home/rasp_user/emh_mqtt
+    ExecStart=/usr/bin/python3 /home/rasp_user/emh_mqtt/emh_mqtt.py
+    Restart=always
+    
+    [Install]
+    WantedBy=multi-user.target
