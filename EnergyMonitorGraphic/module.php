b736749059fd1506f61799180d7d395a66e97da3
MvH <assembla@periscope.de>
2026-03-26 08:56:55 +0100

    Correcting module name EnergyMonitorGraphic

--- EnergyMonitorGraphic/module.php
+++ EnergyMonitorGraphic/module.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../libs/schlauhaus.php';
 
-class EnergyMonitor extends IPSModule
+class EnergyMonitorGraphic extends IPSModule
 {
     use Schlauhaus;
 
